39937939a5ec8bdbd44777986121d072c17148bc
AbdulazizAmmar <azizammar26@gmail.com>
2024-03-19 15:19:09 +0300

    Migrate

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,18 @@
-# ©  2020 Deltatech
+# ©  2008-2021 Deltatech
 # See README.rst file on addons root folder for license details
 
 
 {
     "name": "Merge Objects",
     "summary": "Base module for merging",
-    "version": "13.0.1.0.0",
+    "version": "16.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Tools",
     "depends": ["base"],
-    "license": "LGPL-3",
-    "data": ["security/merge_security.xml"],
+    "license": "OPL-1",
+    "data": ["security/ir.model.access.csv", "data/ir_config_parameter.xml"],
     "images": ["static/description/main_screenshot.png"],
-    "development_status": "Beta",
+    "development_status": "Mature",
     "maintainers": ["dhongu"],
 }
